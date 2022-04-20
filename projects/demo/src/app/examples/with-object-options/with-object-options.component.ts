1ae76622df637f3b0872c60bc4507866a0dc43e9
w3soto <soto.ms@gmail.com>
2022-04-20 08:14:56 +0200

    Disabled state example

--- projects/demo/src/app/examples/with-object-options/with-object-options.component.ts
+++ projects/demo/src/app/examples/with-object-options/with-object-options.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 
 interface Movie {
   id: number,
-  title: string
+  title: string,
+  disabled?: boolean
 }
 
 
@@ -16,22 +17,20 @@ export class WithObjectOptionsComponent implements OnInit {
 
   options: Movie[] = [
     {id: 1, title: 'Drive My Car'},
-    {id: 2, title: 'Dune'},
+    {id: 2, title: 'Dune', disabled: true},
     {id: 3, title: 'Paris, 13th District'},
     {id: 4, title: 'Twin Peaks'},
     {id: 5, title: 'Being John Malkovich'},
-    {id: 6, title: '2001: A Space Odyssey'},
-    {id: 7, title: 'Star Wars'},
+    {id: 6, title: '2001: A Space Odyssey', disabled: true},
+    {id: 7, title: 'Star Wars', disabled: true},
     {id: 8, title: 'Play Time'},
     {id: 9, title: 'American Beauty'},
     {id: 10, title: 'Big Lebowski'},
   ].sort((a, b) => a.title.localeCompare(b.title));
 
-  singleModel: Movie | null = null;
+  model: Movie[] = [];
 
-  multiModel: Movie[] = [];
-
-  valueMultiModel: number[] = [];
+  valueModel: number[] = [];
 
   readonly: boolean = false;
 
@@ -44,23 +43,20 @@ export class WithObjectOptionsComponent implements OnInit {
   }
 
   setRandomValue() {
-    this.singleModel = this.options[Math.floor(Math.random() * this.options.length)];
-
     const options: Movie[] = [];
     const values: number[] = [];
     while(values.length < 3){
       let o: Movie = this.options[Math.floor(Math.random() * this.options.length)];
-      options.indexOf(o) == -1 && options.push(o);
-      values.indexOf(o.id) == -1 && values.push(o.id);
+      options.indexOf(o) == -1 && !o.disabled && options.push(o);
+      values.indexOf(o.id) == -1 && !o.disabled && values.push(o.id);
     }
-    this.multiModel = options;
-    this.valueMultiModel = values;
+    this.model = options;
+    this.valueModel = values;
   }
 
   clearValue() {
-    this.singleModel = null;
-    this.multiModel = [];
-    this.valueMultiModel = [];
+    this.model = [];
+    this.valueModel = [];
   }
 
 }
